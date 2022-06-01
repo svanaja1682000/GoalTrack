5b222176f61ef1e4d0e2575295eb19f53e0a1a9d
svanaja1682000 <86181266+svanaja1682000@users.noreply.github.com>
2022-06-01 19:12:20 +0530

    Delete GoaltrackBackend directory

--- GoaltrackBackend/controller/goals.js
+++
@@ -1,8 +0,0 @@
-const Goals=require("../models").goals
-// Find all users
-async function getallgoals(){
-const goalss = await Goals.findAll();
-console.log("All users:", JSON.stringify(goalss, null, 2));
-return goalss;
-}
-module.exports = {getallgoals};
